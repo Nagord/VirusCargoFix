176c00623781244e5a8923e6e15d82e0f4719986
DragonFire47 <1redyoung@gmail.com>
2021-03-22 00:18:12 -0400

    Add project files.

---
+++ VirusCargoFix/Patch.cs
@@ -0,0 +1,14 @@
+﻿using HarmonyLib;
+
+namespace VirusCargoFix
+{
+    [HarmonyPatch(typeof(PLShipInfoBase), "SetupShipStats")]
+    class Patch
+    {
+        static void Postfix(PLShipInfoBase __instance)
+        {
+            __instance.MyStats.SetGlobalSlotLimit(ESlotType.E_COMP_VIRUS, 64);
+            __instance.MyStats.SetSlotLimit(ESlotType.E_COMP_VIRUS, 64);
+        }
+    }
+}
---
+++ VirusCargoFix/Plugin.cs
@@ -0,0 +1,20 @@
+﻿using PulsarPluginLoader;
+
+namespace VirusCargoFix
+{
+    public class Plugin : PulsarPlugin
+    {
+        public override string Version => "1.0.1";
+
+        public override string Author => "Dragon";
+
+        public override string LongDescription => "Fixes issue where viruses get into cargo";
+
+        public override string Name => "VirusCargoFix";
+
+        public override string HarmonyIdentifier()
+        {
+            return "Dragon.VirusCargoFix";
+        }
+    }
+}
